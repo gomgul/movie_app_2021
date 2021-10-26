e319730af0437df90d72669e2598042a869e612d
gom <nobrain908@gmail.com>
2021-10-26 17:44:27 +0900

    #path :id

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import './Movie.css';
 import {Link} from 'react-router-dom';
 
-function Movie({ title, year, summary, poster, genres}) {
+function Movie({ id, year, title, summary, poster, genres }) {
     return (
         <div className="movie">
             <Link to={{
-                pathname:'/movie-detail',
+                pathname: `/movie/${id}`,
                 state: {
                     year, title, summary, poster, genres
                 },
@@ -18,11 +18,11 @@ function Movie({ title, year, summary, poster, genres}) {
             <h3 className="movie__title" >{title}</h3>
             <h5 className="movie__year">{year}</h5>
             <ul className="movie__genres">
-                {genres.map((genre, index) => {
-                    return 
-                    <li key={index} className="movie__genre">{genre}</li>;
+                {genres.map((genre, index) => (
                     
-                })}
+                    <li key={index} className="movie__genre">{genre}</li>
+                    
+                ))}
             </ul>    
             <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
